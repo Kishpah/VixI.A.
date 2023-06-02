2bd36e35031f9a02ecc1a6ec0cb732ed06d2d6eb
Iván Núñez <ivan@deicom-technologies.com>
2023-06-02 22:06:00 +0200

    Otra subida

--- src/main.ts
+++ src/main.ts
@@ -1,63 +1,206 @@
-import { Actor, CollisionType, Color, DisplayMode, Engine, Physics, vec } from "excalibur";
+import { Actor, CollisionType, Color, DisplayMode, Engine, ImageSource, Loader, Physics, Sprite, vec } from "excalibur";
 import Resources from "./resources";
 import Image from "./image";
 
-Physics.useRealisticPhysics();
-//Physics.useArcadePhysics();
-Physics.acc = vec(0, 300);
-Physics.checkForFastBodies = true
-Physics.positionIterations = 3
-Physics.velocityIterations = 8
+import imagefile from '/assets/images/hormiga.png';
 
-const game = new Engine({
-    width: 800,
-    height: 600,
-    displayMode: DisplayMode.FitScreen
-});
+class Game extends Engine {
+    constructor() {
+        super({
+            width: 800,
+            height: 600,
+            displayMode: DisplayMode.FitScreen
+        });
+    }
 
-// Caja is an actor made from three boxes, one for the ground and another two, one for each side
-const Caja1 = new Actor({
-    pos: vec(game.halfDrawWidth, game.drawHeight),
-    width: game.drawWidth,
-    height: 100,
-    color: Color.DarkGray,
-    collisionType: CollisionType.Fixed
-});
+    public async initialize() {
+        Physics.useRealisticPhysics();
+        //Physics.useArcadePhysics();
+        Physics.acc = vec(0, 300);
+        Physics.checkForFastBodies = true
+        Physics.positionIterations = 3
+        Physics.velocityIterations = 8
 
-const Caja2 = new Actor({
-    pos: vec(100, game.halfDrawHeight),
-    width: 20,
-    height: game.drawHeight,
-    color: Color.DarkGray,
-    collisionType: CollisionType.Fixed
-});
+        const Caja1 = new Actor({
+            pos: vec(game.halfDrawWidth, game.drawHeight),
+            width: game.drawWidth,
+            height: 100,
+            color: Color.DarkGray,
+            collisionType: CollisionType.Fixed
+        });
 
-const Caja3 = new Actor({
-    pos: vec(game.drawWidth - 200, game.halfDrawHeight),
-    width: 20,
-    height: game.drawHeight,
-    color: Color.DarkGray,
-    collisionType: CollisionType.Fixed
-});
+        const Caja2 = new Actor({
+            pos: vec(100, game.halfDrawHeight),
+            width: 20,
+            height: game.drawHeight,
+            color: Color.DarkGray,
+            collisionType: CollisionType.Fixed
+        });
 
-const Caja4 = new Actor({
-    pos: vec(game.drawWidth - 100, game.halfDrawHeight),
-    width: 200,
-    height: 20,
-    color: Color.DarkGray,
-    collisionType: CollisionType.Fixed
-});
+        const Caja3 = new Actor({
+            pos: vec(game.drawWidth - 200, game.halfDrawHeight),
+            width: 20,
+            height: game.drawHeight,
+            color: Color.DarkGray,
+            collisionType: CollisionType.Fixed
+        });
 
-const Caja5 = new Actor({
-    pos: vec(game.drawWidth, game.halfDrawHeight - game.halfDrawHeight / 2),
-    width: 20,
-    height: game.halfDrawHeight,
-    color: Color.DarkGray,
-    collisionType: CollisionType.Fixed
-});
+        const Caja4 = new Actor({
+            pos: vec(game.drawWidth - 100, game.halfDrawHeight),
+            width: 200,
+            height: 20,
+            color: Color.DarkGray,
+            collisionType: CollisionType.Fixed
+        });
 
-// start-snippet{collision}
-game.start().then(() => {
+        const Caja5 = new Actor({
+            pos: vec(game.drawWidth, game.halfDrawHeight - game.halfDrawHeight / 2),
+            width: 20,
+            height: game.halfDrawHeight,
+            color: Color.DarkGray,
+            collisionType: CollisionType.Fixed
+        });
+
+        // const imagefile = new ImageSource(hormiga);
+
+        // console.log('imagefile: ', imagefile)
+        // game.toggleDebug();
+        const prompt = ['manzana', 'avión', 'árbol', 'hormiga']
+        const resources = await Resources.getResourceList();
+
+        console.log('resources: ', resources
+        )
+        const loader = new Loader(resources.map(r => r.image));
+        await game.start(loader);
+
+        game.currentScene.add(Caja1);
+        game.currentScene.add(Caja2);
+        game.currentScene.add(Caja3);
+        game.currentScene.add(Caja4);
+        game.currentScene.add(Caja5);
+
+        game.currentScene.camera.pos = vec(game.halfDrawWidth, game.halfDrawHeight);
+
+
+        let timeDelay = 0;
+        for (const resource of resources) {
+            // let color: Color;
+            // if (prompt.includes(resource.name)) {
+            //     color = new Color(250, 0, 0)
+            // } else {
+            //     color = new Color(0, 250, 0)
+            // }
+            let color = new Color(0, 250, 0)
+            game.clock.schedule(() => {
+                let element: Image = new Image(game.halfDrawWidth + Math.random() * 40 - 20 - 50, -200, 30, color, resource.name);
+                // If clicked, check if it's valid and move to the right container
+                element.on("pointerdown", () => {
+                    console.log('tag: ', element.tag)
+                    if (prompt.includes(element.tag)) {
+                        moveToRightContainer(element);
+                    }
+                });
+
+                this.currentScene.add(element);
+            }, timeDelay++ * 50);
+
+        }
+
+        const sprite = imagefile.toSprite();
+        let a: Actor = new Actor({
+            pos: vec(game.halfDrawWidth, game.halfDrawHeight),
+            radius: 30,
+            color: Color.Yellow,
+            collisionType: CollisionType.Active,
+        });
+        a.body.bounciness = 0.1;
+        a.collider.useCircleCollider(30);
+        sprite.scale = vec(0.2, 0.2);
+        a.graphics.add(sprite);
+        this.currentScene.add(a);
+
+        function moveToRightContainer(element: Actor) {
+            element.pos = vec(game.drawWidth - 100 + Math.random() * 40 - 20, -100)
+            element.vel = vec(0, 0); // Stop movement
+            element.acc = vec(0, 0);
+        }
+    }
+}
+
+export const game = new Game();
+
+game.initialize();
+
+/*
+async function main() {
+    Physics.useRealisticPhysics();
+    //Physics.useArcadePhysics();
+    Physics.acc = vec(0, 300);
+    Physics.checkForFastBodies = true
+    Physics.positionIterations = 3
+    Physics.velocityIterations = 8
+
+    const game = new Engine({
+        width: 800,
+        height: 600,
+        displayMode: DisplayMode.FitScreen
+    });
+
+    game.showDebug(true);
+
+    // Caja is an actor made from three boxes, one for the ground and another two, one for each side
+    const Caja1 = new Actor({
+        pos: vec(game.halfDrawWidth, game.drawHeight),
+        width: game.drawWidth,
+        height: 100,
+        color: Color.DarkGray,
+        collisionType: CollisionType.Fixed
+    });
+
+    const Caja2 = new Actor({
+        pos: vec(100, game.halfDrawHeight),
+        width: 20,
+        height: game.drawHeight,
+        color: Color.DarkGray,
+        collisionType: CollisionType.Fixed
+    });
+
+    const Caja3 = new Actor({
+        pos: vec(game.drawWidth - 200, game.halfDrawHeight),
+        width: 20,
+        height: game.drawHeight,
+        color: Color.DarkGray,
+        collisionType: CollisionType.Fixed
+    });
+
+    const Caja4 = new Actor({
+        pos: vec(game.drawWidth - 100, game.halfDrawHeight),
+        width: 200,
+        height: 20,
+        color: Color.DarkGray,
+        collisionType: CollisionType.Fixed
+    });
+
+    const Caja5 = new Actor({
+        pos: vec(game.drawWidth, game.halfDrawHeight - game.halfDrawHeight / 2),
+        width: 20,
+        height: game.halfDrawHeight,
+        color: Color.DarkGray,
+        collisionType: CollisionType.Fixed
+    });
+
+    // console.log(document.baseURI)
+    // const imagefile = new ImageSource('./hormiga.png');
+    // console.log('imagefile: ', imagefile)
+    // game.toggleDebug();
+    // const loader = new Loader([imagefile]);
+
+    // start-snippet{collision}
+    await game.start()
+    // console.log('imageFile: ', imagefile)
+
+    //.then(() => {
+
     game.currentScene.add(Caja1);
     game.currentScene.add(Caja2);
     game.currentScene.add(Caja3);
@@ -82,14 +225,6 @@ game.start().then(() => {
         }
         game.clock.schedule(() => {
             let element: Image = new Image(game.halfDrawWidth + Math.random() * 40 - 20 - 50, -200, 30, color, resource.name);
-            // New Circle
-
-
-            // let element: Actor = circle(color);
-            // // Low bounciness
-            // element.body.bounciness = 0.1;
-            // element.collider.useCircleCollider(30);
-
             // If clicked, check if it's valid and move to the right container
             element.on("pointerdown", () => {
                 console.log('tag: ', element.tag)
@@ -100,39 +235,44 @@ game.start().then(() => {
 
             game.currentScene.add(element);
         }, timeDelay++ * 50);
+
+    }
+    // const imagefile = new ImageSource('./assets/images/vaca.png');
+    // const sprite = imagefile.toSprite();
+
+    // let a: Actor = new Actor({
+    //     pos: vec(game.halfDrawWidth, game.halfDrawHeight),
+    //     radius: 100,
+    //     color: Color.Yellow,
+    //     collisionType: CollisionType.Active,
+    //     anchor: vec(0.5, 0.5),
+    // });
+    // a.body.bounciness = 0.1;
+    // a.collider.useCircleCollider(30);
+    // sprite.scale = vec(0.01, 0.01);
+    // a.graphics.add(sprite);
+    // // a.graphics.add(sprite);
+
+    // game.currentScene.add(a);
+
+    // });
+
+    function circle(color: Color) {
+        return new Actor({
+            pos: vec(game.halfDrawWidth + Math.random() * 40 - 20 - 50, -200),
+            radius: 30,
+            color: color, //Color.Yellow,
+            collisionType: CollisionType.Active,
+        });
     }
 
-    // Add 20 circles, one every 1th of a second
-    // for (let i = 0; i < 50; i++) {
-    //     game.clock.schedule(() => {
-    //         // New Circle
-    //         let element: Actor = circle(new Color(Math.random() * 255, Math.random() * 255, Math.random() * 255));
-    //         // Low bounciness
-    //         element.body.bounciness = 0.1;
-    //         element.collider.useCircleCollider(30);
-
-    //         // If clicked, check if it's valid and move to the right container
-    //         element.on("pointerdown", () => {
-    //             moveToRightContainer(element);
-    //         });
-
-    //         game.currentScene.add(element);
-    //     }, i * 50);
-    // }
-
-});
-
-function circle(color: Color) {
-    return new Actor({
-        pos: vec(game.halfDrawWidth + Math.random() * 40 - 20 - 50, -200),
-        radius: 30,
-        color: color, //Color.Yellow,
-        collisionType: CollisionType.Active,
-    });
+    function moveToRightContainer(element: Actor) {
+        element.pos = vec(game.drawWidth - 100 + Math.random() * 40 - 20, -100)
+        element.vel = vec(0, 0); // Stop movement
+        element.acc = vec(0, 0);
+    }
 }
 
-function moveToRightContainer(element: Actor) {
-    element.pos = vec(game.drawWidth - 100 + Math.random() * 40 - 20, -100)
-    element.vel = vec(0, 0); // Stop movement
-    element.acc = vec(0, 0);
-}
+main();
+
+*/
--- src/resources.ts
+++ src/resources.ts
@@ -1,72 +1,68 @@
+import { ImageSource } from "excalibur";
 
+// import imagen from '/assets/images/árbol.png';
 class resources {
     private resources: any[] = [];
 
-    // constructor() {
-    // }
-
-    // public addresource(resource: any) {
-    //     this.resources.push(resource);
-    // }
-
-    // public loadresources(path: string, resourceNames: string[]) {
-    //     resourceNames.forEach((resourceName) => {
-    //         this.addresource(path + resourceName);
-    //     });
-    // }
-
-    public getResourceList() {
+    public async getResourceList() {
         console.log('Getting Resource List')
         this.resources = this.resources.concat([
-            { name: 'pepino', path: '' },
-            { name: 'ajo', path: '' },
-            { name: 'cebolla', path: '' },
-            { name: 'regalo', path: '' },
-            { name: 'balón', path: '' },
-            { name: 'hoguera', path: '' },
-            { name: 'gafas de sol', path: '' },
-            { name: 'tiovivo', path: '' },
-            { name: 'árbol', path: '' },
-            { name: 'tenedor', path: '' },
-            { name: 'gorro de fiesta', path: '' },
-            { name: 'gallina', path: '' },
-            { name: 'pelota de tenis', path: '' },
-            { name: 'huevo', path: '' },
-            { name: 'caramelo', path: '' },
-            { name: 'avión', path: '' },
-            { name: 'hormiga', path: '' },
-            { name: 'tomate', path: '' },
-            { name: 'leche', path: '' },
-            { name: 'pera', path: '' },
-            { name: 'mando', path: '' },
-            { name: 'lechuga', path: '' },
-            { name: 'ovni', path: '' },
-            { name: 'silbato', path: '' },
-            { name: 'oso polar', path: '' },
-            { name: 'maletín', path: '' },
-            { name: 'mundo', path: '' },
-            { name: 'clip', path: '' },
-            { name: 'magdalena', path: '' },
-            { name: 'taza', path: '' },
-            { name: 'vaca', path: '' },
-            { name: 'pasta de dientes', path: '' },
-            { name: 'manzana', path: '' },
-            { name: 'batidora', path: '' },
-            { name: 'flotador', path: '' },
-            { name: 'pila', path: '' },
-            { name: 'moto', path: '' },
-            { name: 'perrito caliente', path: '' },
-            { name: 'hueso', path: '' },
-            { name: 'noria', path: '' },
-            { name: 'prismáticos', path: '' },
-            { name: 'tetera', path: '' },
-            { name: 'bola de espejos', path: '' },
-            { name: 'reloj', path: '' },
-            { name: 'regla', path: '' },
-            { name: 'pato', path: '' },
-            { name: 'palo de golf', path: '' },
-            { name: 'saxofón', path: '' },
+            { name: 'pepino', path: '/assets/images/pepino.png' },
+            { name: 'ajo', path: '/assets/images/ajo.png' },
+            { name: 'cebolla', path: '/assets/images/cebolla.png' },
+            { name: 'regalo', path: '/assets/images/regalo.png' },
+            { name: 'balón', path: '/assets/images/balón.png' },
+            { name: 'hoguera', path: '/assets/images/hoguera.png' },
+            { name: 'gafas de sol', path: '/assets/images/gafas de sol.png' },
+            { name: 'tiovivo', path: '/assets/images/tiovivo.png' },
+            { name: 'árbol', path: '/assets/images/árbol.png' },
+            { name: 'tenedor', path: '/assets/images/tenedor.png' },
+            { name: 'gorro de fiesta', path: '/assets/images/gorro de fiesta.png' },
+            { name: 'gallina', path: '/assets/images/gallina.png' },
+            { name: 'pelota de tenis', path: '/assets/images/pelota de tenis.png' },
+            { name: 'huevo', path: '/assets/images/huevo.png' },
+            { name: 'caramelo', path: '/assets/images/caramelo.png' },
+            { name: 'avión', path: '/assets/images/avión.png' },
+            { name: 'hormiga', path: '/assets/images/hormiga.png' },
+            { name: 'tomate', path: '/assets/images/tomate.png' },
+            { name: 'leche', path: '/assets/images/leche.png' },
+            { name: 'pera', path: '/assets/images/pera.png' },
+            { name: 'mando', path: '/assets/images/mando.png' },
+            { name: 'ovni', path: '/assets/images/ovni.png' },
+            { name: 'silbato', path: '/assets/images/silbato.png' },
+            { name: 'oso', path: '/assets/images/oso.png' },
+            { name: 'maletín', path: '/assets/images/maletín.png' },
+            { name: 'clip', path: '/assets/images/clip.png' },
+            { name: 'magdalena', path: '/assets/images/magdalena.png' },
+            { name: 'taza', path: '/assets/images/taza.png' },
+            { name: 'vaca', path: '/assets/images/vaca.png' },
+            { name: 'pasta de dientes', path: '/assets/images/pasta de dientes.png' },
+            { name: 'manzana', path: '/assets/images/manzana.png' },
+            { name: 'flotador', path: '/assets/images/flotador.png' },
+            { name: 'pila', path: '/assets/images/pila.png' },
+            { name: 'moto', path: '/assets/images/moto.png' },
+            { name: 'hueso', path: '/assets/images/hueso.png' },
+            { name: 'noria', path: '/assets/images/noria.png' },
+            { name: 'prismáticos', path: '/assets/images/prismáticos.png' },
+            { name: 'tetera', path: '/assets/images/tetera.png' },
+            { name: 'reloj', path: '/assets/images/reloj.png' },
+            { name: 'regla', path: '/assets/images/regla.png' },
+            { name: 'saxofón', path: '/assets/images/saxofón.png' },
         ]);
+        // for(let i = 0; i < this.resources.length; i++) {
+        for(let resource of this.resources) {
+            resource.path = '/assets/images/' + resource.name + '.png';
+            console.log('Loading resource: ' + resource.path)
+            //let image = await require(resource.path);
+
+            //let image = await 
+            //import(this.resources[i].path).then((image) => {
+            //console.log('Loaded resource: ' + this.resources[i].path)
+            //this.resources[i].image = new ImageSource(image.default);
+            // resource.image = new ImageSource(imagen.default);
+            // console.log('Added resource: ' + resource.image)
+
+        };
 
         return this.resources;
     }
